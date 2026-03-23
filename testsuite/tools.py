735fe8dbfc93258c4750b6d720c68cf9b078be4e
Arvin Schnell <aschnell@suse.de>
2026-03-23 13:00:39 +0100

    updated test case

--- testsuite/tools.py
+++ testsuite/tools.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 import json
 from mcpserver import McpServer
 
@@ -9,16 +8,17 @@ mcp_server = McpServer()
 
 print("--- Listing Tools ---")
 
-tools_response = mcp_server.send_request("tools/list")
+response = mcp_server.send_request("tools/list")
 
-print(json.dumps(tools_response, indent = 2))
+print(json.dumps(response, indent = 2))
 
-if not tools_response or "result" not in tools_response:
+if not response or "result" not in response:
     raise Exception("Malformed response.")
 
-actual_tools = { tool['name'] for tool in tools_response["result"].get("tools", []) }
+actual_tools = { tool['name'] for tool in response["result"].get("tools", []) }
 
-expected_tools = { "list_configs", "get_config", "list_snapshots", "create_snapshot" }
+expected_tools = { "list_configs", "get_config", "set_config", "list_snapshots",
+                   "create_snapshot", "delete_snapshots" }
 
 if actual_tools != expected_tools:
     print(expected_tools)
