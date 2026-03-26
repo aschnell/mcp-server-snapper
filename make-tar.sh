df03b2fe31641b123528990795b0bf789662802e
Arvin Schnell <aschnell@suse.de>
2026-03-26 15:13:36 +0100

    updated tar generation

--- make-tar.sh
+++ make-tar.sh
@@ -2,6 +2,6 @@
 
 tar -cJvf mcp-server-snapper-0.1.0.tar.xz --exclude='*~'	\
     --transform 's|^|mcp-server-snapper-0.1.0/|'		\
-    LICENSE README.md src/ testsuite/*.py
+    LICENSE README.md src/ testsuite/*.py testsuite/README
 
 mv mcp-server-snapper-0.1.0.tar.xz package/
