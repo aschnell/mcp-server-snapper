7e210200bb308303f3d80c1a30d4a9af43c1e7de
Arvin Schnell <aschnell@suse.de>
2026-07-20 07:45:52 +0200

    - generate verdor.tar.gz

--- make-tar.sh
+++ make-tar.sh
@@ -9,9 +9,13 @@ sed "s/@VERSION@/${VERSION}/g" "$(dirname "$0")/mcp-server-snapper.spec.in" > "$
 # Package tarball
 tar -cJvf mcp-server-snapper-${VERSION}.tar.xz --exclude='*~'	\
     --transform "s|^|mcp-server-snapper-${VERSION}/|"		\
-    LICENSE README.md VERSION src/main.go go.mod go.sum vendor build.sh \
+    LICENSE README.md VERSION src/main.go go.mod go.sum build.sh \
     testsuite/list-configs/main.go testsuite/get-config/main.go testsuite/tools/main.go \
     testsuite/list-snapshots/main.go testsuite/create-snapshot-1/main.go testsuite/create-snapshot-2/main.go \
     testsuite/rollback/main.go testsuite/mcpserver/mcpserver.go testsuite/README
 
+# Package vendor tarball
+tar -czf vendor.tar.gz --exclude='*~' vendor
+
 mv mcp-server-snapper-${VERSION}.tar.xz package/
+mv vendor.tar.gz package/
